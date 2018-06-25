e7102dffcba9e01b967d29b38f9d214e74512318
RumbleFrog <xxlosttechxx@gmail.com>
2018-06-25 09:49:56 -0400

    Thread Traversing

--- main.go
+++ main.go
@@ -27,8 +27,17 @@ var (
 type Section struct {
 	Name         string
 	Href         string
+	Threads      []*Thread
 	ThreadCount  uint64
 	MessageCount uint64
+}
+
+// Thread ...
+type Thread struct {
+	Name    string
+	Href    string
+	Replies uint64
+	Views   uint64
 }
 
 // History ..
@@ -104,6 +113,40 @@ func fetchSections() {
 	})
 
 	c.Visit(target)
+}
+
+func fetchThreads(s *Section) {
+	c := colly.NewCollector()
+
+	c.OnRequest(onRequest)
+	c.OnError(onError)
+
+	var (
+		r   uint64
+		v   uint64
+		err error
+	)
+
+	c.OnHTML("li.discussionListItem", func(e *colly.HTMLElement) {
+		if r, err = strconv.ParseUint(e.ChildText("div.stats > dl.major > dd"), 10, 64); err != nil {
+			return
+		}
+
+		if v, err = strconv.ParseUint(e.ChildText("div.stats > dl.minor > dd"), 10, 64); err != nil {
+			return
+		}
+
+		s.Threads = append(s.Threads, &Thread{
+			Name:    e.ChildText("div.main > div.titleText > h3.title > a[href].PreviewToolTip"),
+			Href:    e.ChildAttr("div.main > div.titleText > h3.title > a[href].PreviewToolTip", "href"),
+			Replies: r,
+			Views:   v,
+		})
+	})
+
+	c.OnHTML("div.PageNav nav > a[href].text", func(e *colly.HTMLElement) {
+		e.Request.Visit(e.Attr("href"))
+	})
 }
 
 func onRequest(r *colly.Request) {
